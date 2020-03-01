c1cab7af9c345f187cf086953d41a02f2990244b
Gundars Vasilevskis <gundars772@gmail.com>
2020-03-01 12:28:05 +0100

    Fix not found handler for refreshing

--- company-api/src/application.go
+++ company-api/src/application.go
@@ -4,12 +4,17 @@ import (
 	"company-api/src/controller"
 	"company-api/src/helper"
 	"company-api/src/repository"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 	"log"
 	"net/http"
 	"os"
 )
+
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("Not found test")
+}
 
 func main() {
 
@@ -23,11 +28,13 @@ func main() {
 	controller.InitCompanyController(router)
 	controller.InitOwnerController(router)
 
-	router.NotFoundHandler = http.HandlerFunc(controller.NotFound)
-
 	router.Use(helper.InitLoggingMiddleware)
 
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./web/")))
+	router.PathPrefix("/company").Handler(http.FileServer(http.Dir("./web/")))
+	router.PathPrefix("/owner").Handler(http.FileServer(http.Dir("./web/")))
+
+	router.NotFoundHandler = http.HandlerFunc(NotFound)
 
 	log.Println("Server now listening at :" + os.Getenv("PORT"))
 	repository.InitRepository()
--- company-api/src/controller/controller.go
+++ company-api/src/controller/controller.go
@@ -2,13 +2,8 @@ package controller
 
 import (
 	"gopkg.in/go-playground/validator.v9"
-	"net/http"
 )
 
 var (
 	validate = validator.New()
 )
-
-func NotFound(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/", http.StatusSeeOther)
-}
