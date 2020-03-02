5daf71434d3f7e2c80aa96ac6ba6e91fb67e4d4d
Gundars Vasilevskis <gundars772@gmail.com>
2020-03-02 13:50:27 +0100

    Added company owner add and remove functionality

--- company-web/src/components/ownerDialog/ownerDialog.tsx
+++ company-web/src/components/ownerDialog/ownerDialog.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import {
-	Avatar,
+	Avatar, CircularProgress,
 	Dialog,
 	DialogContent,
 	DialogTitle,
@@ -21,6 +21,8 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import {IOwner} from "../owner/ownerInterfaces";
 import axios from "axios";
+import {useSnackbar} from "notistack";
+import AddIcon from '@material-ui/icons/Add';
 
 interface IOwnerDialogProps {
 	company?: ICompany,
@@ -28,9 +30,18 @@ interface IOwnerDialogProps {
 	handleClose: () => void;
 }
 
+enum AddDelete {
+	AddOwner = 0,
+	DeleteOwner = 1
+}
+
 const OwnerDialog: React.FC<IOwnerDialogProps> = (props) => {
-	const [owners, setOwners] = React.useState<IOwner[]>();
+	const [dropdownOwners, setDropdownOwners] = React.useState<IOwner[]>();
+	const [allOwners, setAllOwners] = React.useState<IOwner[]>();
+	const [companyOwners, setCompanyOwners] = React.useState<IOwner[]>(props.company.Owners);
 	const [loading, setLoading] = React.useState(true);
+	const { enqueueSnackbar } = useSnackbar();
+	const [ownerSelected, setOwnerSelected] = React.useState<number>();
 
 	const loadOwners = async () => {
 		setLoading(true);
@@ -38,16 +49,84 @@ const OwnerDialog: React.FC<IOwnerDialogProps> = (props) => {
 		await axios.get("/api/owner").then(res => {
 			if (props.company.Owners === undefined || props.company.Owners === null) {
 				const allOwners: IOwner[] = res.data;
-				setOwners(allOwners);
+				setAllOwners(allOwners);
+				setDropdownOwners(allOwners);
 			} else {
-				const allOwners: IOwner[] = res.data;
-				const availableOwners = allOwners.filter((owner: IOwner) => props.company.Owners.some((currentOwner: IOwner) => owner.ID !== currentOwner.ID));
-				setOwners(availableOwners);
+				setAllOwners(res.data);
+				setDropdownOwners(filterOwners(res.data));
 			}
-
 		});
-
 		setLoading(false);
+	};
+
+	const filterOwners = (allOwners:IOwner[]): IOwner[] => {
+		return allOwners.filter(owner => !props.company.Owners.some(o => owner.ID == o.ID));
+	};
+
+	const updateCompanyOwners = (owner: IOwner, addDelete: AddDelete) => {
+		let changedDropdown = dropdownOwners;
+		let changedOwners = companyOwners;
+		switch (addDelete) {
+			case AddDelete.AddOwner:
+				changedDropdown.splice(changedDropdown.findIndex((o) =>{
+					return o.ID === owner.ID;
+				}), 1);
+				setDropdownOwners(changedDropdown);
+				changedOwners = [...changedOwners, owner];
+				setCompanyOwners(changedOwners);
+				props.company.Owners = changedOwners;
+				break;
+			case AddDelete.DeleteOwner:
+				changedDropdown = [...changedDropdown, owner];
+				setDropdownOwners(changedDropdown);
+				changedOwners = changedOwners.splice(changedDropdown.findIndex((o) =>{
+					return o.ID === owner.ID;
+				}), 1);
+				setCompanyOwners(changedOwners);
+				props.company.Owners = changedOwners;
+				break;
+			default:
+				break;
+		}
+	};
+
+	const deleteOwnerFromCompany = async (owner: IOwner) => {
+		await axios.delete("/api/company/" + props.company.ID + "/owner/" + owner.ID).then(response => {
+			if (response.status === 200) {
+				setLoading(true);
+				updateCompanyOwners(owner, AddDelete.DeleteOwner);
+				enqueueSnackbar("Owner was added to company", {variant: "success"});
+				setLoading(false);
+				return;
+			}
+			enqueueSnackbar("Owner was not deleted from company", {variant: "error"});
+			return;
+		})
+	};
+
+
+
+	const addOwnerToCompany = async () => {
+		if (ownerSelected === undefined)
+		{
+			enqueueSnackbar("Owner is not selected", {variant: "warning"});
+			return;
+		}
+		await axios.put("/api/company/" + props.company.ID + "/owner/" + ownerSelected).then(response => {
+			if (response.status === 200) {
+				setLoading(true);
+				updateCompanyOwners(getOwnerByID(allOwners, ownerSelected), AddDelete.AddOwner);
+				enqueueSnackbar("Owner was added to company", {variant: "success"});
+				setLoading(false);
+				return;
+			}
+			enqueueSnackbar("Owner was not added to company", {variant: "error"});
+			return;
+		})
+	};
+
+	const getOwnerByID =(array: IOwner[], ownerID: number):IOwner=> {
+		return array.find(o => ownerID == o.ID);
 	};
 
 	React.useEffect(() => {
@@ -58,9 +137,10 @@ const OwnerDialog: React.FC<IOwnerDialogProps> = (props) => {
 		<Dialog open={props.open} onClose={props.handleClose}>
 			<DialogTitle id="simple-dialog-title">{props.company.Name + " owners"}</DialogTitle>
 			<DialogContent>
-				<List dense={false}>
+				{
+					!loading ? <List dense={false}>
 					{
-						props.company.Owners ? props.company.Owners.map((owner: IOwner) => {
+						companyOwners ? companyOwners.map((owner: IOwner) => {
 							return <ListItem key={owner.ID}>
 								<ListItemAvatar>
 									<Avatar>
@@ -73,7 +153,7 @@ const OwnerDialog: React.FC<IOwnerDialogProps> = (props) => {
 								/>
 								<ListItemSecondaryAction>
 									<IconButton edge="end" aria-label="delete" onClick={() => {
-										console.log(owner.ID)
+										deleteOwnerFromCompany(owner);
 									}
 									}>
 										<DeleteIcon/>
@@ -83,21 +163,27 @@ const OwnerDialog: React.FC<IOwnerDialogProps> = (props) => {
 						}) : null
 					}
 					<ListItem>
-						<FormControl fullWidth={true}>
-							<InputLabel htmlFor="grouped-select">Add Owner</InputLabel>
-							<Select onChange={object => {
-								console.log(object.target.value);
-							}} input={<Input id="grouped-select"/>}>
+						<FormControl style={{display:"inline"}} fullWidth={true}>
+							<InputLabel htmlFor="select">Add owner</InputLabel>
+							<Select style={{width: "87%"}} defaultValue="" input={<Input id="select" disabled={dropdownOwners.length == 0} onChange={(object) => setOwnerSelected(parseInt(object.target.value))} />}>
 								{
-									owners ? owners.map((owner: IOwner) => {
-										return <MenuItem
-											value={owner.ID}>{owner.FirstName + " " + owner.LastName.charAt(0) + "."}</MenuItem>
-									}) : null
+									dropdownOwners ? dropdownOwners.map((owner: IOwner) => {
+											const ownerString:string = owner.FirstName + " " + owner.LastName.charAt(0) + ".";
+											return <MenuItem key={owner.ID}
+												value={owner.ID}>{ownerString}</MenuItem>
+										}) : null
 								}
 							</Select>
+							<IconButton disabled={dropdownOwners.length == 0} edge="end" aria-label="delete" onClick={() => addOwnerToCompany()}>
+								<AddIcon/>
+							</IconButton>
 						</FormControl>
 					</ListItem>
-				</List>
+					</List> : <div style={{ marginTop: 100, marginBottom: 100, textAlign: "center" }}>
+						<CircularProgress size={40}/>
+					</div>
+				}
+
 			</DialogContent>
 		</Dialog>
 	)
